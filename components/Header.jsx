0cbeb76ee5ecc95c6f5c3401583e3e34f6ada272
mr-banner <sahukamalkant28@gmail.com>
2025-10-27 22:20:53 +0530

    .

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,8 +4,11 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
+  
   return (
     <div className="fixed top-0 w-full bg-white/20 backdrop-blur-3xl z-50 shadow-md shadow-slate-300/80">
       <nav className="container mx-auto px-7 md:px-15 py-4 flex items-center justify-between">
@@ -21,40 +24,38 @@ const Header = () => {
 
         <div className="flex items-center space-x-4">
           <SignedIn>
-            <Link href="/dashboard" className="text-gray-600 flex items-center gap-2">
-              <Button
-                className="cursor-pointer bg-transparent hover:bg-white hover:shadow-[#d5caff] hover:shadow-xl/80 transition-all duration-500 ease-in-out hover:border-[#7f5efd] hover:text-[#7f5efd]"
-                variant="outline"
-              >
-                <LayoutDashboard size={18} />
+            <Link href={"/dashboard"}
+            className="text-gray-600 flex items-center gap-2"
+            >
+              <Button className="cursor-pointer bg-transparent hover:bg-white hover:shadow-[#d5caff] hover:shadow-xl/80 transition-all duration-500 ease-in-out hover:border-[#7f5efd] hover:text-[#7f5efd]" variant={"outline"}>
+                <LayoutDashboard size={18}/>
                 <span className="hidden md:inline">Dashboard</span>
               </Button>
             </Link>
-
-            <Link href="/transaction/create" className="text-gray-600 flex items-center gap-2">
+            <Link href={"/transaction/create"}
+            className="text-gray-600  flex items-center gap-2"
+            >
               <Button className="cursor-pointer hover:shadow-xl/80 hover:shadow-[#d5caff] bg-[#7f5efd] hover:bg-[#7f5efd] transition-all duration-500 ease-in-out">
-                <PenBox size={18} />
+                <PenBox size={18}/>
                 <span className="hidden md:inline">Transaction</span>
               </Button>
             </Link>
-
-            <UserButton
-              appearance={{
-                elements: {
-                  avatarBox: "h-10 w-10",
-                  avatar: "h-10 w-10",
-                },
-              }}
-            />
           </SignedIn>
-
           <SignedOut>
             <SignInButton forceRedirectUrl="/dashboard">
-              <Button className="cursor-pointer" variant="outline">
+              <Button className="cursor-pointer" variant={"outline"}>
                 Login
               </Button>
             </SignInButton>
           </SignedOut>
+          <SignedIn>
+            <UserButton appearance={{
+              elements:{
+                avatarBox: "h-10 w-10",
+                avatar: "h-10 w-10",
+              }
+            }}/>
+          </SignedIn>
         </div>
       </nav>
     </div>
